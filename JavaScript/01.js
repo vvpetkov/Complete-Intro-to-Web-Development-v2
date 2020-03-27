6f6e2c14ddd767ee2954d35f2f42b07096978248
unknown <vladimir.v.petkov@gmail.com>
2020-03-27 16:47:48 +0200

    Update

---
+++ JavaScript/01.js
@@ -0,0 +1,10 @@
+const person = {
+    name:'Vladimir Petkov',
+    city: 'Sofia',
+    favoriteFood:'Mozzarella',
+    wantsSomeFoodRightNow: true,
+    numberOfMozzarella:100
+};
+
+console.log (person)
+cons
